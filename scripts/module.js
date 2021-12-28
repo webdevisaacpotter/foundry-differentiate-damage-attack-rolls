6cd004f80801419eae4a9eda14f88468a7e97217
Isaac <webdevisaacpotter@gmail.com>
2021-12-28 16:31:18 -0500

    Added emojis to css instead of JS injection

--- scripts/module.js
+++ scripts/module.js
@@ -3,13 +3,13 @@ Hooks.on("preCreateChatMessage", (a, b, c) => {
 
   if(a.data.flags.dnd5e.roll.type === "attack") {
     console.log("%cATK","color:#CCCCCC");
-    var newFlavourHTML = "<span class='atk-flavor-txt'>⚔️" + a.data.flavor + "</span>"
+    var newFlavourHTML = "<span class='atk-flavor-txt'>" + a.data.flavor + "</span>"
     a.data.flavor = newFlavourHTML;
     a.data._source.flavor = newFlavourHTML;
   } 
   else if(a.data.flags.dnd5e.roll.type === "damage") {
     console.log("%cDMG","color:red");
-    var newFlavourHTML = "<span class='dmg-flavor-txt'>🩸" + a.data.flavor + "</span>"
+    var newFlavourHTML = "<span class='dmg-flavor-txt'>" + a.data.flavor + "</span>"
     a.data.flavor = newFlavourHTML;
     a.data._source.flavor = newFlavourHTML;
   } 
